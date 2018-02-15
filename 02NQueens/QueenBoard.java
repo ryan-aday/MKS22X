b0df5ae3281422323dae0721819aa8116e0e4adf
ryan-aday <raday@stuy.edu>
2018-02-15 15:19:14 -0500

    Fixing add, and remove, sorryit's so late

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,9 +1,13 @@
 public class QueenBoard{
     public static void main(String[]args){
-	QueenBoard a=new QueenBoard(2);
+	QueenBoard a=new QueenBoard(3);
+	a.addQueen(1,1);
 	System.out.println(a.toString());
-	//a.addQueen(0,0);
-   	//System.out.println(a.toString());
+	a.addQueen(1,0);
+   	System.out.println(a.toString());
+	
+	a.removeQueen(1,1);
+   	System.out.println(a.toString());
 	/*	a.addQueen(0,0);
    	System.out.println(a.toString());
 	a.removeQueen(0,0);
@@ -12,7 +16,7 @@ public class QueenBoard{
 	a.removeQueen(0,0);
    	System.out.println(a.toString());
 	*/
-	System.out.println(a.solve());
+	//System.out.println(a.solve());
 	//	System.out.println(a.toString());
 	//	System.out.println(a.countSolutions());
 	//	System.out.println(a.toString());
@@ -59,10 +63,10 @@ public class QueenBoard{
 	if (r>=board.length || c>=board.length){
 	    throw new IllegalStateException();
 	}
-	if(board[r][c]<0){
+	if(board[r][c]!=0){
 	    return false;
 	}
-	if (r>0 && r<board.length-1){
+	/*	if (r>0 && r<board.length-1){
 	    if (c>0 && c<board.length-1){
 		if (board[r-1][c]==1 ||
 		    board[r+1][c]==1 ||
@@ -76,16 +80,30 @@ public class QueenBoard{
 		}
 	    }
 	}
+	*/
 
-	for (int check=0; check<board.length; check++){
-	    if (board[r][check]==1 || board[check][c]==1){
-		return false;
-	    }
-	}
 
 	board[r][c]=1;
+
+	for (int urow=r-1; urow>-1; urow--){
+	    if(board[urow][c]!=1){
+		board[urow][c]=board[urow][c]-1;
+	    }
+	}
 	
-	for (int col=r; col<board.length; col++){
+	for (int row=r+1; row<board.length; row++){
+	    if(board[row][c]!=1){
+		board[row][c]=board[row][c]-1;
+	    }
+	}
+	
+	for (int ucol=c-1; ucol>-1; ucol--){
+	    if(board[r][ucol]!=1){
+		board[r][ucol]=board[r][ucol]-1;
+	    }
+	}
+	
+	for (int col=c+1; col<board.length; col++){
 	    if(board[r][col]!=1){
 		board[r][col]=board[r][col]-1;
 	    }
@@ -95,37 +113,119 @@ public class QueenBoard{
 	for (int dr=r+1; dr<board.length; dr++){
 	    dc++;
 	    if (dc>=board.length){
-	    }else if(board[dr][dc]!=1){
-		board[dr][dc]=board[dr][dc]-1;
+		if(board[dr][dc]!=1){
+		    board[dr][dc]=board[dr][dc]-1;
+		}else{}
+	    }
+	}
+
+	int uc=c;
+	for (int ur=r-1; ur>-1; ur--){
+	    uc--;
+	    if (uc>=0){
+		if(board[ur][uc]!=1){
+		    board[ur][uc]=board[ur][uc]-1;
+		}
+	    }else{}
+	}
+	
+	int ddc=c;
+	for (int ddr=r+1; ddr<board.length; ddr++){
+	    ddc--;
+	    if (dc>=board.length){
+		if(board[ddr][ddc]!=1){
+		    board[ddr][ddc]=board[ddr][ddc]-1;
+		}else{}
+	    }
+	}
+
+	int uuc=c;
+	for (int uur=r-1; uur>-1; uur--){
+	    uuc++;
+	    if (uc>=0){
+		if(board[uur][uuc]!=1){
+		    board[uur][uuc]=board[uur][uuc]-1;
+		}
 	    }else{}
 	}return true;
     }
    
     private boolean removeQueen(int r, int c){
-	if (r>board.length || c>board.length){
+	if (r>=board.length || c>=board.length){
 	    throw new IllegalStateException();
 	}
-	if (board[r][c]<1){
+	if(board[r][c]!=1){
 	    return false;
 	}
+
 	board[r][c]=0;
-	
-	for (int col=r; col<board.length; col++){
-	    if(board[r][col]<0){
-		board[r][col]=board[r][col]+1;
+
+	for (int urow=r-1; urow>-1; urow--){
+	    if(board[urow][c]!=1){
+		board[urow][c]=board[urow][c]+1;
 	    }
 	}
 	
+	for (int row=r+1; row<board.length; row++){
+	    if(board[row][c]!=1){
+		board[row][c]=board[row][c]+1;
+	    }
+	}
+	
+	for (int ucol=c-1; ucol>-1; ucol--){
+	    if(board[r][ucol]!=1){
+		board[r][ucol]=board[r][ucol]+1;
+	    }
+	}
+	
+	for (int col=c+1; col<board.length; col++){
+	    if(board[r][col]!=1){
+		board[r][col]=board[r][col]+1;
+	    }
+	}
+
 	int dc=c;
 	for (int dr=r+1; dr<board.length; dr++){
 	    dc++;
 	    if (dc>=board.length){
-	    }else if(board[dr][dc]<0){
-		board[dr][dc]=board[dr][dc]+1;
+		if(board[dr][dc]!=1){
+		    board[dr][dc]=board[dr][dc]+1;
+		}else{}
+	    }
+	}
+
+	int uc=c;
+	for (int ur=r-1; ur>-1; ur--){
+	    uc--;
+	    if (uc>=0){
+		if(board[ur][uc]!=1){
+		    board[ur][uc]=board[ur][uc]+1;
+		}
+	    }else{}
+	}
+	
+	int ddc=c;
+	for (int ddr=r+1; ddr<board.length; ddr++){
+	    ddc--;
+	    if (dc>=board.length){
+		if(board[ddr][ddc]!=1){
+		    board[ddr][ddc]=board[ddr][ddc]+1;
+		}else{}
+	    }
+	}
+
+	int uuc=c;
+	for (int uur=r-1; uur>-1; uur--){
+	    uuc++;
+	    if (uc>=0){
+		if(board[uur][uuc]!=1){
+		    board[uur][uuc]=board[uur][uuc]+1;
+		}
 	    }else{}
 	}return true;
-	
     }
+
+    
 
     /**
      *@return false when the board is not solveable and leaves the board filled with zeros; 
