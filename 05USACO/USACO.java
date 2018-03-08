5209894e2ad207a99ac3dc2ab4f7ce646c50c2d7
ryan-aday <raday@stuy.edu>
2018-03-08 15:32:36 -0500

    Still working on silver, need to work on instantiating files

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class USACO{
     public static void main(String[] args){
-	System.out.println(silver("ctravel.10.in")); 
+	System.out.println(silver("ctravel.1.in")); 
 	
     }
     
@@ -12,50 +12,37 @@ public class USACO{
     }
 
     public static int silver(String filename){
-	int rcount=0;
-	int ccount=0;
 	
-        //instead of a try/catch, you can throw the FileNotFoundException.
-        File text = new File("input.txt");// can be a path like: "/full/path/to/file.txt" 
-	char[][] data;
+        File text = new File(filename);
+        
+
+	int sizeX=0;
+	int sizeY=0;
+	int travel=0;
+
+	int startX=0;
+	int startY=0;
+	int endX=0;
+	int endY=0;
 	
-        //inf stands for the input file
 	try {
 	    Scanner inf = new Scanner(text);
-	    Scanner actual = new Scanner(text);
+	    Scanner in= new Scanner(text);
 
-	    int row=-1;
-	    String ch="";
-
-	    while (inf.hasNextLine()){
-		rcount++;
-		ch=inf.nextLine();
-	    }
-	    ccount=ch.length();
-	    data=new char[rcount][ccount];
+	    int lnCount=0;
+	    while (
 	    
-	    // System.out.println(board.length+" "+board[0].length);
-
-	    while (actual.hasNextLine()){
-		row++;
-		String line=actual.nextLine();
-		for (int count=0; count<line.length(); count++){
-		    data[row][count]=line.charAt(count);
+	    for (int count=0; count<; count++){
+		String dataC= inf.next();
+		
+		if (dataC.substring(0,1)!="." || line.substring(0,1)!="*"){
+		    
 		}
+		System.out.println(line);
 	    }
-	    
-	    String test="";
-	    for (int rw=0; rw<data.length; rw++){
-		test=test+"\n";
-		for (int col=0; col<data[rw].length; col++){
-		    test=test+data[rw][col];
-		}
-	    }
-	    System.out.println(test);
-	    
-	
+		
 	}catch (FileNotFoundException e){
 	    return -1;
-	}return -1;
+	}return 0;
     }
 }
