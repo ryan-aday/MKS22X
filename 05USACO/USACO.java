305a8532831edd0306ee09b5d8b6f8d92453b098
ryan-aday <raday@stuy.edu>
2018-03-09 15:32:23 -0500

    Need to change the scanner, needs debugging

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -14,7 +14,12 @@ public class USACO{
     public static int silver(String filename){
 	
         File text = new File(filename);
-        
+
+        String[] dataS;
+	char[][] data;
+	int[] dataE;
+
+	dataE=new int[7];
 
 	int sizeX=0;
 	int sizeY=0;
@@ -24,23 +29,53 @@ public class USACO{
 	int startY=0;
 	int endX=0;
 	int endY=0;
+
 	
-	try {
+	
+	try {	    
 	    Scanner inf = new Scanner(text);
 	    Scanner in= new Scanner(text);
+	    Scanner i=new Scanner(text);
 
 	    int lnCount=0;
-	    while (
+	    int lnSize=0;
+	    int dataCount=0;
+	    int lneCount=0;
 	    
-	    for (int count=0; count<; count++){
-		String dataC= inf.next();
-		
-		if (dataC.substring(0,1)!="." || line.substring(0,1)!="*"){
-		    
-		}
-		System.out.println(line);
+	    while (inf.hasNextLine()){
+		lnCount++;
+
+		dataCount++;
+
+		System.out.println(lnCount+" "+dataCount);
 	    }
-		
+
+	    while (i.hasNextLine()){
+		lneCount++;
+		String ln=inf.nextLine();
+		if (lneCount>0 && lneCount<dataCount){
+		    lnSize=ln.length();
+		    System.out.println(lnSize);
+		}
+	    }
+
+	    dataS=new String[7];
+	    
+	    int Ecount=0;
+	    while (inf.hasNextInt()){
+		dataE[Ecount]=inf.nextInt();
+		Ecount++;
+	    }
+
+	    data=new char[dataE[0]][dataE[1]];
+	    
+	    for (int count=1; count<dataS.length-1; count++){
+		for (int ct=0; ct<lnSize; ct++){
+		    if (dataS[count].charAt(ct)!= '*' || dataS[count].charAt(ct)!= '.'){
+			data[count][ct]=dataS[count].charAt(ct);
+		    }
+		}
+	    }
 	}catch (FileNotFoundException e){
 	    return -1;
 	}return 0;
