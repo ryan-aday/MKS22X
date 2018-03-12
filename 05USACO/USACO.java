3e734a45630f5f45bd38e76379cc441798c70fda
ryan-aday <raday@stuy.edu>
2018-03-12 07:20:29 -0400

    Wrote silver, still need to add a sum method

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -81,72 +81,70 @@ public class USACO{
 
 	
     public static int silver(String filename){
-	
-        File text = new File(filename);
+	File f = new File(filename);
+	try {
+	    Scanner inf = new Scanner(f);
+	    String[] data = inf.nextLine().split(" ");
+	    int row = Integer.parseInt(data[0]);
+	    int col = Integer.parseInt(data[1]);
+	    int moves = Integer.parseInt(data[2]);
+	    char[][] board = new char[row][col];
 
-        String[] dataS;
-	char[][] data;
-	int[] dataE;
-
-	dataE=new int[7];
-
-	int sizeX=0;
-	int sizeY=0;
-	int travel=0;
-
-	int startX=0;
-	int startY=0;
-	int endX=0;
-	int endY=0;
-
-	
-	
-	try {	    
-	    Scanner inf = new Scanner(text);
-	    Scanner in= new Scanner(text);
-	    Scanner i=new Scanner(text);
-
-	    int lnCount=0;
-	    int lnSize=0;
-	    int dataCount=0;
-	    int lneCount=0;
-	    
-	    while (inf.hasNextLine()){
-		lnCount++;
-
-		dataCount++;
-
-		System.out.println(lnCount+" "+dataCount);
-	    }
-
-	    while (i.hasNextLine()){
-		lneCount++;
-		String ln=inf.nextLine();
-		if (lneCount>0 && lneCount<dataCount){
-		    lnSize=ln.length();
-		    System.out.println(lnSize);
+	    for (int r=0; r<row; r++){
+		String line = inf.nextLine();
+		for (int c=0; c<col; c++){
+		    board[r][c] = line.charAt(c);
 		}
 	    }
 
-	    dataS=new String[7];
-	    
-	    int Ecount=0;
-	    while (inf.hasNextInt()){
-		dataE[Ecount]=inf.nextInt();
-		Ecount++;
-	    }
-
-	    data=new char[dataE[0]][dataE[1]];
-	    
-	    for (int count=1; count<dataS.length-1; count++){
-		for (int ct=0; ct<lnSize; ct++){
-		    if (dataS[count].charAt(ct)!= '*' || dataS[count].charAt(ct)!= '.'){
-			data[count][ct]=dataS[count].charAt(ct);
+	    String[] coor = in.nextLine().split(" ");
+	    int sr = Integer.parseInt(coor[0]);
+	    int sc = Integer.parseInt(coor[1]);
+	    int er = Integer.parseInt(coor[2]);
+	    int ec = Integer.parseInt(coor[3]);
+	    int[][] oldHelp= new int[row][col];
+	    int[][] newHelp= new int[row][col];
+	  
+	    for (int ro= 0; ro<row; ro++){
+		for (int co=0; co<col; co++){
+		    if (board[ro][co] == '*'){
+			oldHelp[ro][co] = -1;
+			newHelp[ro][co] = -1;
+		    }
+		    else if (ro==(sr-1) && co== (sc-1)){
+		        oldHelp[ro][co]=1;
+			newHelp[ro][co]=1;
+		    }
+		    else {
+			oldHelp[ro][co] = 0;
+			newHelp[ro][co] = 0;
 		    }
 		}
 	    }
-	}catch (FileNotFoundException e){
-	    return -1;
-	}return 0;
+
+	    for (int m=0; m<moves; m++){
+		for (int r=0; r<row; r++){
+		    for (int c = 0;c < col;c++){
+			oldHelp[r][c] =  newHelp[r][c];
+		    }
+		}
+		for (int r=0; r<row; r++){
+		    for (int c=0; c<col; c++){
+			try{
+			    if (newHelp[r][c]!=-1){
+			    }
+			}catch (ArrayIndexOutOfBoundsException e){
+			}
+		    }
+		}
+	    }
+	    return  newHelp[er-1][ec-1];
+	} catch (FileNotFoundException e){
+	    System.exit(1);
+	}
+	return -1;
     }
+    
+    
+
 }
