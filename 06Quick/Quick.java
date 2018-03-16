af8f8918722f756e8fb393fcbed9bc912f0d6cb4
ryan-aday <raday@stuy.edu>
2018-03-16 10:42:05 -0400

    StackOOverflow Error, still trying to figure out why

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,26 +1,51 @@
 import java.util.*;
+import java.lang.*;
 
 public class Quick{
     public static int partition(int[]data, int start, int end){
 	Random rand = new Random();
-	int pivotI=rand.nextInt(end - 1)+start;
-	int pivot=data[pivotI];
-	swap(data,pivotI,start);
-	int s=start+1;
-	int l=end-1;
-	while (s<=l){
-	    System.out.println(pivot);
-	    System.out.println(toString(data));
-	    if (data[s] <= pivot){
-		s++;
+	/*
+	int step1= end-start+1;
+	int step2= rand.nextInt()%step1;
+	int pivotI=(start+step2);
+
+
+	*/
+
+	int i=start;
+	int le=start+1;
+	int ge=end;
+	
+	while (i<data.length && i>0 && i>=start  && i<=end){
+
+	    int pivotI=rand.nextInt(end-start+1)+start;
+	    //System.out.println(pivotI);
+	    int pivot=data[pivotI];  //How do I keep getting 12?
+	    //System.out.println(data[pivotI]);
+	    swap(data,pivotI,start);
+	    
+
+	    
+	    while (i<=ge && i>=0 && le!=ge-1){
+		if (data[i]==pivot){
+		    i++;
+		}else if (data[i]>pivot){
+		    swap(data, i, ge);
+		    ge--;
+		}else{
+		    swap(data, i, le);
+		    i++;
+		le++;
+		}
 	    }
-	    else if (data[s]>pivot){
-		swap(data,s,l);
-		l--;
-	    }
-	}
-	swap(data,s,l);
-	return l;
+	    
+	    //System.out.println(i);
+	    //System.out.println(le);
+	    //System.out.println(ge);
+	    
+	    partition(data, i, ge);
+	    partition(data, le, i-1);
+	}return i;
     }
 
     public static String toString(int[] i){
@@ -37,7 +62,7 @@ public class Quick{
 	data[end]=temp;
     }
 
-    public static int quickSelect(int[] data, int k){
+    public static int quickselect(int[] data, int k){
 	return quickHelp(data, k, 0, data.length-1);
     }
 
@@ -68,6 +93,17 @@ public class Quick{
 	    }*/
     }
 
+<<<<<<< HEAD
+    public static void quicksort(int[] a){
+	    sortHelp(a, 0, a.length - 1);
+    }
+
+    public static void sortHelp(int[] a, int start,int end){
+        if (start<end){
+	    int pivot=partition(a,start,end);
+	    sortHelp(a, pivot+1, end);   //Code is no longer viable
+	    sortHelp(a, start, pivot-1);
+=======
     public static void quickSort(int[] a){
 	sortHelp(a, 0, 0, a.length - 1);
     }
@@ -87,10 +123,17 @@ public class Quick{
 		partition (data, start, i-1);
 		partition (data, i+1, end);
 	    }
+>>>>>>> 8d57762662b1a148e1a89e2388a67336fd9e90ef
 	}
     }
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+	int[] ary = new int[]{999,999,999,4,1,0,3,2,999,999,999};
+	//System.out.println(quickselect(ary,2));
+	quicksort(ary);
+	//System.out.println(partition(ary, 0, ary.length-1));
+	System.out.println(toString(ary));
+    }
 
 }
 
