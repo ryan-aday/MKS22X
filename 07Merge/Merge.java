348c8c1a1ead1468ace6573ed9d9b34eb94239ab
ryan-aday <raday@stuy.edu>
2018-03-21 21:16:45 -0400

    Rewrote parameters for mergesort, cleaned up code

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,22 +1,19 @@
 public class Merge{
     //private int[] temp;
 
-    public static void mergesort(int[] data, int start, int end){
-	//Finding half of data
+    public static void mergesort(int[] data){
+	mergesort(data, 0, data.length-1);
+    }
+
+    public static void mergeSort(int[] data, int start, int end){
 	if (start>=end){
 	    return ;
 	}
-	/*
-	for (int count=start; count<end; count++){
-	    temp[count]=data[count];
-	}
-	*/
-	//	System.out.println(toString(data)+"\n"+toString(temp)+"\n");
+
 	int mid= ((end-start)/2)+start;
-	//	System.out.println(mid);
 	
-	mergesort(data, start, mid);
-	mergesort(data, mid+1, end);
+	mergeSort(data, start, mid);
+	mergeSort(data, mid+1, end);
 	merge(data, start, mid, end);
     }
     
@@ -29,7 +26,6 @@ public class Merge{
         for (int da=0; da<tempTwo.length; da++)
             tempTwo[da] = data[mid+1+da];
 
-	//System.out.println("Working...");
 	int c=0;
 	int d=0;
 	int e=start;
@@ -54,53 +50,6 @@ public class Merge{
 	    d++;
 	    e++;
 	}
-	
-	/*while (c<mid && d<end){
-	    if (data[c]>data[c+1]){
-		//swap(data, c, c+1);
-		temp[c-start]=data[c];
-	    }
-	    if (data[d]>data[d+1]){
-		swap(data, d, d+1);
-		temp[d-start]=data[d];
-	    }
-	    if (data[d]<data[c]){
-		swap(data, c, d);
-		temp[c-start]=data[c];
-	    }
-	    c++;
-	    d++;
-	}
-	data=temp;
-	*/
-	/*
-	int min;
-	for (int c=start; c<end+1; c++){
-	    temp[c]=data[c];
-	}
-	for (int count=start; count<end; count++){
-	    min= count;
-	    for (int counts=count+1; counts<data.length; counts++){
-		if (data[counts]<data[min]){
-		    min=counts;
-		}
-	    }swap(temp, count, min);
-	}
-	*/
-	/*
-	for (int count=1; count<temp.length; count++){
-	    int min=temp[count];
-	    int comp=count-1;
-	    while(comp >=0  && temp[comp]>min){
-		temp[comp+1]=temp[comp];
-		comp--;
-	    }
-	    temp[comp+1]=min;
-	}
-	for (int d=start; d<end+1; d++){
-	    data[d]=temp[d-start];
-	}
-	*/
     }
 
     public static String toString(int[] i){
@@ -110,17 +59,10 @@ public class Merge{
 	}
 	return str+"\n";
     }
-    
-    public static void swap(int[] data, int i, int j){
-	int temp=data[i];
-	data[i]=data[j];
-	data[j]=temp;
-    }
 
     public static void main(String[] args){
 	int[] ary= {23,1274,231,324,23,23,9112,19,10};
-	int[] temp= new int[2];
-	mergesort(ary, 0, ary.length-1);
+	mergesort(ary);
 	System.out.println(toString(ary));
     }
 }
