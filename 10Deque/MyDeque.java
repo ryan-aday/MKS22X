ed25eb9b745a7e85bc776c1a0d9ee54dcf3b3c0f
ryan-aday <raday@stuy.edu>
2018-04-19 15:34:57 -0400

    Working on add

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -4,17 +4,41 @@ import java.util.*;
 public class MyDeque<E>{
     private E[] data;
     //private E first, last;
-    private int size;
+    private int size, first, last;
 
+    public static void main(String[]args){
+	MyDeque<Integer> a= new MyDeque<Integer>();
+	a.addFirst(12);
+	System.out.println(a.toString());
+	a.addFirst(12);
+	System.out.println(a.toString());
+	a.addFirst(12);
+	System.out.println(a.toString());
+	a.addFirst(12);
+	System.out.println(a.toString());
+
+	
+    }
+
+    public String toString(){
+	String str="";
+	for (int c=0; c<data.length; c++){
+	    str=str+" "+data[c];
+	}
+	return str;
+    }
+    
     @SuppressWarnings("unchecked")
     public MyDeque(){
-	data=(E[])new LinkedList [10];
+	data=(E[])new Object[10];
 	size=0;
+	first=0;
+	last=0;
     }
 
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
-	data=(E[])new LinkedList[initialCapacity];
+	data=(E[])new Object[initialCapacity];
 	size=0;
     }
 
@@ -27,19 +51,52 @@ public class MyDeque<E>{
 	    throw new NullPointerException();
 	}else if(size()==data.length){
 	    System.out.println("WHY");
+	}else if (size()==0){
+	    data[0]=val;
+	    last=1;
+	}else if(first<last){
+	    for (int d=first; d>0; d--){
+		if (data[d]!=null){
+		    data[d]=val;
+		    first=d;
+		}
+	    }
 	}else{
-	    data.push(val);
-        }
+	    for (int c=data.length-1; c>first+1; c--){	
+		if (data[c]!=null){
+		    data[c]=val;
+		    first=c;
+		}
+	    }
+	}
     }
+    
     public void addLast(E val){
 	if (val==null){
 	    throw new NullPointerException();
 	}else if(size()==data.length){
 	    System.out.println("WHY");
+
+	}else if (size()==0){
+	    data[0]=val;
+	    last=1;
+	}else if(last>first){
+	    for (int d=last; d>data.length; d++){
+		if (data[d]!=null){
+		    data[d]=val;
+		    last=d;
+		}
+	    }
 	}else{
-	    data.pop(val);
-        }
+	    for (int c=0; c<last; c++){	
+		if (data[c]!=null){
+		    data[c]=val;
+		    last=c;
+		}
+	    }
+	}
     }
+    
     //throw new NullPointerException();  if the specified element is null and this deque does not permit null elements
 
     /*
