9761450f11b9083fc32108f0789b0f0e31b2c1f3
ryan-aday <raday@stuy.edu>
2018-04-23 09:01:11 -0400

    Finished 10

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -3,129 +3,149 @@ import java.util.*;
 
 public class MyDeque<E>{
     private E[] data;
-    //private E first, last;
-    private int size, first, last;
-
-    public static void main(String[]args){
-	MyDeque<Integer> a= new MyDeque<Integer>();
-	a.addFirst(12);
-	System.out.println(a.toString());
-	a.addFirst(12);
-	System.out.println(a.toString());
-	a.addFirst(12);
-	System.out.println(a.toString());
-	a.addFirst(12);
-	System.out.println(a.toString());
-
-	
-    }
-
-    public String toString(){
-	String str="";
-	for (int c=0; c<data.length; c++){
-	    str=str+" "+data[c];
-	}
-	return str;
-    }
+    private int f,b;
+    private int size;
     
     @SuppressWarnings("unchecked")
     public MyDeque(){
-	data=(E[])new Object[10];
-	size=0;
-	first=0;
-	last=0;
+	data = (E[])new Object[10];
+	f=0;
+	b=0;
     }
 
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
-	data=(E[])new Object[initialCapacity];
-	size=0;
+	if (initialCapacity < 0){
+	    throw new IllegalArgumentException();
+	}
+	data = (E[]) new Object[initialCapacity];
+	f=0;
+	b=0;
     }
 
     public int size(){
 	return size;
     }
 
-    public void addFirst(E val){
-	if (val==null){
-	    throw new NullPointerException();
-	}else if(size()==data.length){
-	    System.out.println("WHY");
-	}else if (size()==0){
-	    data[0]=val;
-	    last=1;
-	}else if(first<last){
-	    for (int d=first; d>0; d--){
-		if (data[d]!=null){
-		    data[d]=val;
-		    first=d;
-		}
-	    }
-	}else{
-	    for (int c=data.length-1; c>first+1; c--){	
-		if (data[c]!=null){
-		    data[c]=val;
-		    first=c;
-		}
-	    }
+    @SuppressWarnings("unchecked")
+   private void resize(){
+	E[] temp = (E[]) new Object[size() * 2];
+	for (int c=0; c<size(); c++){
+	    temp[c] = data[c];
 	}
+	data=temp;
+	f=0;
+	b=size() - 1;
     }
     
-    public void addLast(E val){
-	if (val==null){
+    public void addFirst(E element){
+	if (element == null){
 	    throw new NullPointerException();
-	}else if(size()==data.length){
-	    System.out.println("WHY");
-
-	}else if (size()==0){
-	    data[0]=val;
-	    last=1;
-	}else if(last>first){
-	    for (int d=last; d>data.length; d++){
-		if (data[d]!=null){
-		    data[d]=val;
-		    last=d;
-		}
+	}
+	if(size == data.length){
+		resize();
 	    }
-	}else{
-	    for (int c=0; c<last; c++){	
-		if (data[c]!=null){
-		    data[c]=val;
-		    last=c;
-		}
+	if (size == 0){
+	    data[f] = element;
+	}
+	else{
+	    if (f <= 0){
+	        f = f + data.length - 1;
+		data[f] = element;
+	    }
+	    else{
+		front--;
+		data[f] = element;
 	    }
 	}
+	size++;
     }
-    
-    //throw new NullPointerException();  if the specified element is null and this deque does not permit null elements
 
-    /*
+    public void addLast(E element){
+	if (size == data.length){
+	    resize();
+	}
+	if (element == null){
+	    throw new NullPointerException();
+	}
+	if (size==0){
+	    data[b] = element;
+	}
+	else {
+	    if (b >= data.length - 1){
+		b = b - (data.length - 1);
+		data[b] = element;
+	    }
+	    else{
+		b++;
+		data[b] = element;
+	    }
+	}
+	size++;
+    }
+
     public E removeFirst(){
-	if (size()==0){
+	if (size() == 0){
 	    throw new NoSuchElementException();
 	}
+	E temp=data[front];
+	data[f]=null;
+	if (f==data.length - 1){
+	    f=0;
+	}
+	else{
+	    f++;
+	}
+        size--;
+	return temp;
     }
+
     public E removeLast(){
 	if (size()==0){
 	    throw new NoSuchElementException();
 	}
+	E temp=data[b];
+	data[b]=null;
+	if (b==0){
+	    b=data.length - 1;
+	}
+	else {
+	    b-=1;
+	}
+        size-=1;
+	return temp;
     }
-    //throw new NoSuchElementException(); 
-	
+
     public E getFirst(){
 	if (size()==0){
 	    throw new NoSuchElementException();
 	}
+	return data[f];
     }
+
     public E getLast(){
 	if (size()==0){
 	    throw new NoSuchElementException();
 	}
+	return data[b];
     }
 
-    */
+    public String toString(){
+    String ans = "[";
+    if(f < b){
+      for (int c=f; c< b; c++){
+        ans += data[c] + " , ";
+      }
+    }
+    else{
+      for(int c=f; c<data.length; c++){
+        ans += data[c] + ", ";
+      }
+      for(int c=0; c<=b; c++){
+        ans += data[c] + ", ";
+      }
+    }
+    ans = ans.substring(0, ans.length() - 2) + "]";
+    return ans;
+  }
 }
-    //throw new NoSuchElementException(); 
-
-
-    //resize will demand a suppress
