b0605f95d0039a04c04d5c89a821d81db3b1565c
ryan-aday <raday@stuy.edu>
2018-05-04 12:29:57 -0400

    Worked on some methods

--- 14MazeSolver/Location.java
+++ 14MazeSolver/Location.java
@@ -2,6 +2,18 @@ public class Location{
     private int x,y;
     private Location previous;
 
-    public Location(int _x, int _y, Location prev){
+    public Location(int Lx, int Ly, Location prev){
+    }
+
+    public int getX(){
+	return x;
+    }
+
+    public int getY(){
+	return y;
+    }
+
+    public Location getPrev(){
+	return previous;
     }
 }
