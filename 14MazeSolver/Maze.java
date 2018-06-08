bf2f21a2f725e28a2de06fda2579187ca20f757c
ryan-aday <raday@stuy.edu>
2018-06-08 07:34:02 -0400

    Fixed aStar issue and colorize

--- 14MazeSolver/Maze.java
+++ 14MazeSolver/Maze.java
@@ -1,199 +1,199 @@
 import java.io.*;
 import java.io.FileNotFoundException;
 import java.util.*;
+
 public class Maze{
-    private static final String CLEAR_SCREEN =  "\033[2J";
-    private static final String HIDE_CURSOR =  "\033[?25l";
-    private static final String SHOW_CURSOR =  "\033[?25h";
-    Location start,end;
-    private char[][]maze;
-    private boolean AStar;
-    
+  private static final String CLEAR_SCREEN =  "\033[2J";
+  private static final String HIDE_CURSOR =  "\033[?25l";
+  private static final String SHOW_CURSOR =  "\033[?25h";
+  Location start,end;
+  private char[][]maze;
+  private boolean aStar;
+
+  /*
+    YOU MUST COMPLETE THIS METHOD!!!
+    YOU MUST COMPLETE THIS METHOD!!!
+    YOU MUST COMPLETE THIS METHOD!!!
+  */
+
+  public void setAStar(boolean value){
+    aStar = value;
+  }
+
+  public Location[] getNeighbors(Location L){
+
+    Location[] neighbors = new Location[4];
+    int x = L.getX();
+    int y = L.getY();
+    int size = 0;
+    int nDist = 0;
+    if (aStar){
+      nDist = L.getSoFar()+1;
+    }
+    if (isValid(x-1,y)){
+      neighbors[size] = new Location(x-1,y,L,Math.abs(x-1-end.getX())+Math.abs(y-end.getY()),nDist);
+      size++;
+    }
+    if (isValid(x+1,y)){
+      neighbors[size] = new Location(x+1,y,L,Math.abs(x+1-end.getX())+Math.abs(y-end.getY()),nDist);
+      size++;
+    }
+    if (isValid(x,y-1)){
+      neighbors[size] = new Location(x,y-1,L,Math.abs(x-end.getX())+Math.abs(y-1-end.getY()),nDist);
+      size++;
+    }
+    if (isValid(x,y+1)){
+      neighbors[size] = new Location(x,y+1,L,Math.abs(x-end.getX())+Math.abs(y+1-end.getY()),nDist);
+      size++;
+    }
+    return neighbors;
+  }
+
+  private boolean isValid (int x, int y){
+    return x>=0 && x<maze.length && y>=0 && y<maze[0].length &&
+      (maze[x][y] == ' ' || maze[x][y] == 'E');
+  }
+
+  public Location getStart(){
+    return start;
+  }
+  public Location getEnd(){
+    return end;
+  }
+
+
+  private static String go(int x,int y){
+    return ("\033[" + x + ";" + y + "H");
+  }
+  private static String color(int foreground,int background){
+    return ("\033[0;" + foreground + ";" + background + "m");
+  }
+
+  public void clearTerminal(){
+    System.out.println(CLEAR_SCREEN+"\033[1;1H");
+  }
+  public Maze(String filename){
+    ArrayList<char[]> lines = new ArrayList<char[]>();
+    int startr=-1, startc=-1;
+    int endr=-1,endc=-1;
+    try{
+      Scanner in = new Scanner(new File(filename));
+      while(in.hasNext()){
+        lines.add(in.nextLine().toCharArray());
+      }
+    }catch(FileNotFoundException e){
+      System.out.println("File not found: "+filename);
+      System.exit(1);
+    }
+    maze = new char[lines.size()][];
+    for(int i = 0; i < maze.length; i++){
+      maze[i]=lines.get(i);
+    }
+    for(int r=0; r<maze.length;r++){
+      for(int c=0; c<maze[r].length;c++){
+        if(maze[r][c]=='S'){
+          if(startr == -1){
+            startr=r;
+            startc=c;
+          }else{
+            System.out.println("Multiple 'S' found!");
+            System.exit(0);
+          }
+        }
+
+        if(maze[r][c]=='E'){
+          //erase E
+          //maze[r][c]=' ';
+          if(endr == -1){
+            endr=r;
+            endc=c;
+          }else{
+            System.out.println("Multiple 'E' found!");
+            System.exit(0);
+          }
+        }
+      }
+    }
+    if(startr == -1 || endr == -1){
+      System.out.println("Missing 'S' or 'E' from maze.");
+      System.exit(0);
+
+    }
+
     /*
-      YOU MUST COMPLETE THIS METHOD!!!
-      YOU MUST COMPLETE THIS METHOD!!!
-      YOU MUST COMPLETE THIS METHOD!!!
+      THIS IS AN IMPORTANT PART BECAUSE YOU WILL NEED TO CHANGE IT LATER!
+      The start/end Locations may need more information later when we add
+      other kinds of frontiers!
     */
-    
-    public void setAStar(boolean value){
-	AStar=value;
-    }
+    end=new Location(endr,endc,null,0,0);
+    start=new Location(startr,startc,null,0,0);
+    aStar=false;
+  }
 
-    public Location[] getNeighbors(Location L){
-	Location[] neighbors = new Location[4];
-	int x = L.getX();
-	int y = L.getY();
-	int size = 0;
-	int newDist=0;
-	if (AStar==true){
-	    newDist=L.getSoFar();
-	}
-	if (isValid(x-1,y)){
-	    neighbors[size] = new Location(x-1, y, L, Math.abs(x-1-end.getX())+Math.abs(y-end.getY()), newDist);
-	    size++;
-	}
-	if (isValid(x+1,y)){
-	    neighbors[size] = new Location(x+1, y, L, Math.abs(x-1-end.getX())+Math.abs(y-end.getY()), newDist);
-	    size++;
-	}
-	if (isValid(x,y-1)){
-	    neighbors[size] = new Location(x, y-1, L, Math.abs(x-end.getX())+Math.abs(y-1-end.getY()), newDist);
-	    size++;
-	}
-	if (isValid(x,y+1)){
-	    neighbors[size] = new Location(x, y+1, L, Math.abs(x-end.getX())+Math.abs(y+1-end.getY()), newDist);
-	    size++;
-	}
-	return neighbors;
-    }
-    
-    private boolean isValid (int x, int y){
-	return x>=0 && x<maze.length && y>=0 && y<maze[0].length &&
-	    (maze[x][y]==' '||maze[x][y] == 'E');
-    }
-    
-    
-    public Location getStart(){
-	return start;
-    }
-    public Location getEnd(){
-	return end;
-    }
+  public String toStringColor(){
+    return toStringColor(50);
+  }
 
-    //End of edited code    
+  public String toStringColor(int delay){
+    try{
+      Thread.sleep(delay);
+    }catch(InterruptedException e){
 
-    private static String go(int x,int y){
-	return ("\033[" + x + ";" + y + "H");
     }
-    private static String color(int foreground,int background){
-	return ("\033[0;" + foreground + ";" + background + "m");
+    return HIDE_CURSOR+CLEAR_SCREEN+go(1,1)+colorize(toString())+SHOW_CURSOR;
+  }
+
+  public String toString(){
+    int maxr = maze.length;
+    int maxc = maze[0].length;
+    String ans = "";
+    for(int i = 0; i < maxr * maxc; i++){
+      int row = i/maxc;
+      int col = i%maxc;
+
+      char c =  maze[row][col];
+      ans+=c;
+      if( col == maxc-1 ){
+        ans += "\n";
+      }
+
     }
-    
-    public void clearTerminal(){
-	System.out.println(CLEAR_SCREEN+"\033[1;1H");
+    return ans + "\n";
+  }
+
+  public char get(int row,int col){
+    return maze[row][col];
+  }
+  public void set(int row,int col, char n){
+    maze[row][col] = n;
+  }
+  public static String colorize(String s){
+    String ans = "";
+    Scanner in = new Scanner(s);
+    while(in.hasNext()){
+      String line ="";
+      for(char c : in.nextLine().toCharArray()){
+        if(c == '#'){
+          line+= color(37,47)+c;
+        }
+        else if(c == '@'){
+          line+= color(33,40)+c;
+        }
+        else if(c == '?'){
+          line+= color(37,42)+c;
+        }
+        else if(c == '.'){
+          line+= color(36,40)+c;
+        }
+        else if(c == ' '){
+          line+= color(35,40)+c;
+        }else{
+          line+=color(37,40)+c;
+        }
+
+      }
+      ans += line+color(37,40)+"\n";
     }
-    public Maze(String filename){
-	ArrayList<char[]> lines = new ArrayList<char[]>();
-	int startr=-1, startc=-1;
-	int endr=-1,endc=-1;
-	try{
-	    Scanner in = new Scanner(new File(filename));
-	    while(in.hasNext()){
-		lines.add(in.nextLine().toCharArray());
-	    }
-	}catch(FileNotFoundException e){
-	    System.out.println("File not found: "+filename);
-	    System.exit(1);
-	}
-	maze = new char[lines.size()][];
-	for(int i = 0; i < maze.length; i++){
-	    maze[i]=lines.get(i);
-	}
-	for(int r=0; r<maze.length;r++){
-	    for(int c=0; c<maze[r].length;c++){
-		if(maze[r][c]=='S'){
-		    if(startr == -1){
-			startr=r;
-			startc=c;
-		    }else{
-			System.out.println("Multiple 'S' found!");
-			System.exit(0);
-		    }
-		}
-		
-		if(maze[r][c]=='E'){
-		    //erase E
-		    //maze[r][c]=' ';
-		    if(endr == -1){
-			endr=r;
-			endc=c;
-		    }else{
-			System.out.println("Multiple 'E' found!");
-			System.exit(0);
-		    }
-		}
-	    }
-	}
-	if(startr == -1 || endr == -1){
-	    System.out.println("Missing 'S' or 'E' from maze.");
-	    System.exit(0);
-	    
-	}
-	
-	/*
-	  THIS IS AN IMPORTANT PART BECAUSE YOU WILL NEED TO CHANGE IT LATER!
-	  The start/end Locations may need more information later when we add
-	  other kinds of frontiers!
-	*/
-	end = new Location(endr,endc,null,0,0);
-	start = new Location(startr,startc,null,0,0);
-	AStar=false;
-    }
-    
-    public String toStringColor(){
-	return toStringColor(50);
-    }
-    
-    public String toStringColor(int delay){
-	try{
-	    Thread.sleep(delay);
-	}catch(InterruptedException e){
-	    
-	}
-	return HIDE_CURSOR+CLEAR_SCREEN+go(1,1)+colorize(toString())+SHOW_CURSOR;
-    }
-    
-    public String toString(){
-	int maxr = maze.length;
-	int maxc = maze[0].length;
-	String ans = "";
-	for(int i = 0; i < maxr * maxc; i++){
-	    int row = i/maxc;
-	    int col = i%maxc;
-	    
-	    char c =  maze[row][col];
-	    ans+=c;
-	    if( col == maxc-1 ){
-		ans += "\n";
-	    }
-	    
-	}
-	return ans + "\n";
-    }
-    
-    public char get(int row,int col){
-	return maze[row][col];
-    }
-    public void set(int row,int col, char n){
-	maze[row][col] = n;
-    }
-    public static String colorize(String s){
-	String ans = "";
-	Scanner in = new Scanner(s);
-	while(in.hasNext()){
-	    String line ="";
-	    for(char c : in.nextLine().toCharArray()){
-		if(c == '#'){
-		    line+= color(37,47)+c;
-		}
-		else if(c == '@'){
-		    line+= color(33,40)+c;
-		}
-		else if(c == '?'){
-		    line+= color(37,42)+c;
-		}
-		else if(c == '.'){
-		    line+= color(36,40)+c;
-		}
-		else if(c == ' '){
-		    line+= color(35,40)+c;
-		}else{
-		    line+=color(37,40)+c;
-		}
-		
-	    }
-	    ans += line+color(37,40)+"\n";
-	}
-	return ans;
-    }
+    return ans;
+  }
 }
--- 14MazeSolver/MazeSolver.java
+++ 14MazeSolver/MazeSolver.java
@@ -1,95 +1,103 @@
 import java.util.*;
+
 public class MazeSolver{
-    private Maze maze;
-    private Frontier frontier;
-    private boolean animate;
-    
-    public static void main(String[]args){
-		
-	MazeSolver a=new MazeSolver("test3.txt");
-	System.out.println(a);
-	a.setAnimate(true);
-	a.solve(3);
-	System.out.println(a);
-	
+    /*
+  public static void main(String[]args){
+    MazeSolver m = new MazeSolver("data1.dat");
+    m.setAnimate(false);
+    m.solve();
+    System.out.println(m.toString());
+  }
+    */
+
+  private Maze maze;
+  private Frontier frontier;
+    private boolean hasAttempted, willAnimate;
+
+  public MazeSolver(String fileName){
+    maze = new Maze(fileName);
+    willAnimate = false;
+  }
+
+  private void wait(int millis){
+    try {
+      Thread.sleep(millis);
     }
-    
-    public MazeSolver(String fileName){
-	maze=new Maze(fileName);
-	animate=false;
+    catch (InterruptedException e) {
     }
-    
-    private void wait(int time){
-	try {
-	    Thread.sleep(time);
-	}
-	catch (InterruptedException e) {
-	}
+  }
+
+  public void setAnimate(boolean val){
+    willAnimate = val;
+  }
+  //Default to BFS
+  public boolean solve(){ return solve(1); }
+
+  //mode: required to allow for alternate solve modes.
+  //0: BFS
+  //1: DFS
+  //2: Best-First Search// Hungry Search
+  //3: A Star
+  public boolean solve(int mode){
+    //initialize your frontier
+    if (mode == 1){
+      frontier = new FrontierStack();
     }
-    
-    public void setAnimate(boolean bool){
-	animate=bool;
+    else if (mode == 0) {
+      frontier = new FrontierQueue();
     }
-    //Default to BFS
-    public boolean solve(){ return solve(0); }
-    
-    //mode: required to allow for alternate solve modes.
-    //0: BFS
-    //1: DFS
-    //2: PriS
-    //3: ASS
-    public boolean solve(int mode){
-	//initialize your frontier
- 	
-	if (mode == 1){
-	  frontier=new FrontierStack();
-	}else if (mode == 0) {
-	    frontier=new FrontierQueue();
-	}else if (mode == 2){
-	    frontier=new FrontierPriorityQueue();
-	}else if (mode == 3){
-	    frontier=new FrontierPriorityQueue();
-	    maze.setAStar(true);
-	}
-	frontier.add(maze.getStart());
-	Location end = maze.getEnd();
-	while (frontier.hasNext()){
-	    if (animate){
-		System.out.println("\033[2J\033[1;1H");
-		System.out.println(this);
-		wait(30);
-	    }
-	    Location next = frontier.next();
-	    maze.set(next.getX(),next.getY(),'.');
-	    Location[] newL = maze.getNeighbors(next);
-	    for (int a=0; a<newL.length; a++){
-		Location c = newL[a];		
-		if (c != null){
-		    if (c.equals(end)){
-			maze.end = new Location(maze.end.getX(),maze.end.getY(),c.getPrev(),0, 0);
-			maze.set(maze.getEnd().getX(),maze.getEnd().getY(),'E');
-			return true;
-		    }
-		    frontier.add(c);
-		    maze.set(c.getX(),c.getY(),'?');
-		}
-	    }
-	}
-	return false;
+    else if (mode == 2){
+      frontier = new FrontierPriorityQueue();
     }
-    
-    public String toString(){
-	Location c = maze.getEnd().getPrev();
-	while (c!=null){
-	    maze.set(c.getX(), c.getY(), '@');
-	    c=c.getPrev();
-	    if (animate){
-		System.out.println("\033[2J\033[1;1H");
-		System.out.println(maze.colorize(maze.toString()));
-		wait(30);
-	    }
-	}
-	maze.set(maze.start.getX(),maze.start.getY(),'S');
-	return maze.colorize(maze.toString());
+    else if (mode == 3){
+      frontier = new FrontierPriorityQueue();
+      maze.setAStar(true);
     }
+    frontier.add(maze.getStart());
+    Location end = maze.getEnd();
+    //while there is stuff in the frontier:
+    //  get the next location
+    //  process the location to find the locations (use the maze to do this)
+    //  check if any locations are the end, if you found the end just return true
+    //  add all the locations to the frontier
+    //when there are no more values in the frontier return false
+    while (frontier.hasNext()){
+      if (willAnimate){
+        System.out.println("\033[2J\033[1;1H");
+        System.out.println(this);
+        wait(30);
+      }
+      Location next = frontier.next();
+      maze.set(next.getX(),next.getY(),'.');
+      Location[] newLocations = maze.getNeighbors(next);
+      for (int i=0; i<newLocations.length; i++){
+        Location cur = newLocations[i];
+        if (cur != null){
+          if (cur.equals(end)){
+            maze.end = new Location(maze.end.getX(),maze.end.getY(),cur.getPrev(),0,0);
+            maze.set(maze.getEnd().getX(),maze.getEnd().getY(),'E');
+            return true;
+          }
+          frontier.add(cur);
+          maze.set(cur.getX(),cur.getY(),'?');
+        }
+      }
+    }
+    return false;
+  }
+
+  public String toString(){
+    Location c = maze.getEnd().getPrev();
+    while (c!=null){
+      maze.set(c.getX(),c.getY(),'@');
+      c=c.getPrev();
+      if (willAnimate){
+        System.out.println("\033[2J\033[1;1H");
+        System.out.println(maze.colorize(maze.toString()));
+        wait(30);
+      }
+    }
+    maze.set(maze.start.getX(),maze.start.getY(),'S');
+    return maze.toString();
+  }
 }
